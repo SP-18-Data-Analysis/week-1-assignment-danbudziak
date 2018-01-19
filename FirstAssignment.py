361777596840e6cd0e343e7b8df38da32ed7f740
Daniel Budziak <danielbudziak@lewisu.edu>
2018-01-19 13:07:03 -0600

    First Assignment for Data Analysis Programming

---
+++ FirstAssignment.py
@@ -0,0 +1,19 @@
+#Here is my list 
+#My list includes all the classes that I am currently taking this semester.
+myClasses = ["Introduction to Data Mining", "Programming for Data Analysis", "Applied Linear Algebra", "Object-Oriented Programming", "Cultural Diversity"]
+
+#Printing out the entire list
+print(myClasses)
+
+#Printing out this class that I am submitting the assignment for
+print(myClasses[1])
+
+#Initialize a map
+danBudziak = {0:"21 Years Old", 1:"Junior Year", 2:"Computer Science Major at Lewis"}
+
+#Printing out the map that describes me
+print(danBudziak)
+
+#Printing out the first element in the map
+print(danBudziak[0])
+
